d65d04ed3d50aeb84a728cc0c6fe96b8ad2b5d1d
Andreas Røssland <andreas.roessland@gmail.com>
2022-05-22 14:04:25 +0200

    download all demos necessary for regression tests

--- scripts/regression-tests.sh
+++ scripts/regression-tests.sh
@@ -3,6 +3,13 @@
 set -e
 
 scripts_dir=$(dirname "$0")
-$scripts_dir/download-test-data.sh default.7z unexpected_end_of_demo.7z regression-set.7z
+$scripts_dir/download-test-data.sh\
+    default.7z\
+    unexpected_end_of_demo.7z\
+    regression-set.7z\
+    retake_unknwon_bombsite_index.7z\
+    broken.7z\
+    overtime-demos.7z\
+    valve_matchmaking.7z
 
 go test -tags unassert_panic ./...
